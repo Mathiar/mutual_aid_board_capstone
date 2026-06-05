86308a8dc6e0d4849d0fe9a9bc7fbe655b0cd404
Justin R <rybacki.justin@student.clackamas.edu>
2026-06-05 14:27:08 -0700

    Removed trailing / from frontend URL

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://mutual-aid-board-capstone-1.onrender.com/'  // Replace with YOUR frontend URL
+    'https://mutual-aid-board-capstone-1.onrender.com'  // Replace with YOUR frontend URL
   ],
   credentials: true
 }));
